668d54a8756a89ce76453bef5842819c144b0760
wjy1214 <136069600+wjy1065676928@users.noreply.github.com>
2025-08-21 06:04:22 +0800

    Delete out directory

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP PREEMPT Wed Aug 20 07:52:47 PDT 2025"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.2), GNU ld (GNU Binutils for Ubuntu) 2.34"
