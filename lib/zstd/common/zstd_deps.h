e5416707c92104915d29fec7e3c53dd866fcc006
wjy1214 <136069600+wjy1065676928@users.noreply.github.com>
2025-08-21 12:09:30 +0800

    Update zstd_deps.h

--- lib/zstd/common/zstd_deps.h
+++ lib/zstd/common/zstd_deps.h
@@ -1,8 +1,6 @@
-#ifndef UNUSED_ATTR
-#define UNUSED_ATTR __attribute__((unused))
-#endif
 #ifndef ZSTD_DEPS_H
 #define ZSTD_DEPS_H
+
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/errno.h>
@@ -11,37 +9,48 @@
 #include <linux/bug.h>
 #include <linux/byteorder/generic.h>
 #include <asm/unaligned.h>
+
+#ifndef UNUSED_ATTR
+#define UNUSED_ATTR __attribute__((unused))
+#endif
+
 #ifndef ZSTD_STATIC_ASSERT
 # define ZSTD_STATIC_ASSERT(c) BUILD_BUG_ON(!(c))
 #endif
 #ifndef DEBUG_STATIC_ASSERT
 # define DEBUG_STATIC_ASSERT(c) BUILD_BUG_ON(!(c))
 #endif
+
 #define ZSTD_readLE16(p)      get_unaligned_le16(p)
 #define ZSTD_readLE24(p)      (get_unaligned_le32(p) & 0xFFFFFFu)
 #define ZSTD_readLE32(p)      get_unaligned_le32(p)
 #define ZSTD_readLE64(p)      get_unaligned_le64(p)
 #define ZSTD_writeLE32(p,v)   put_unaligned_le32((v),(p))
+
 #define ZSTD_malloc(s)        kmalloc((s), GFP_KERNEL)
 #define ZSTD_calloc(n,s)      kcalloc((n),(s), GFP_KERNEL)
 #define ZSTD_realloc(p,s)     krealloc((p),(s), GFP_KERNEL)
 #define ZSTD_free(p)          kfree((p))
+
 #ifndef ZSTD_DEBUG
 # define ZSTD_DEBUG 0
 #endif
+
 #if ZSTD_DEBUG
 # define ZSTD_LOG(...)        pr_debug(__VA_ARGS__)
 #else
 # define ZSTD_LOG(...)        do { } while (0)
 #endif
+
 #define ZSTD_MULTITHREAD      0
-#endif
+
 #ifndef INLINE_KEYWORD
 #define INLINE_KEYWORD static inline
 #endif
 #ifndef FORCE_INLINE_TEMPLATE
 #define FORCE_INLINE_TEMPLATE static inline
 #endif
+
 #ifndef ZSTD_HIGHBIT32_DEFINED
 #define ZSTD_HIGHBIT32_DEFINED
 static inline unsigned ZSTD_highbit32(unsigned val) {
@@ -52,6 +61,7 @@ static inline unsigned ZSTD_highbit32(unsigned val) {
 static inline unsigned ZSTD_countTrailingZeros32(unsigned val) {
     return __builtin_ctz(val);
 }
+
 #ifndef assert
 #define assert(expr) BUILD_BUG_ON(!(expr))
 #endif
